b6687675763e7b123a7f44dd7039f704dc2fa8d2
Randynot <prempehagyemang62@gmail.com>
2026-08-06 20:35:57 +0000

    configure the cloudinary service

--- src/core/cloudinary/cloudinary.module.ts
+++ src/core/cloudinary/cloudinary.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 
+import { CloudinaryConfig } from '../../shared/config/cloudinary.config';
+
 @Module({
-  providers: [],
+  providers: [CloudinaryConfig],
+  exports: [CloudinaryConfig],
 })
 export class cloudinary {}
