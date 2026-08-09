b57daf52e8b2dc97b64c3d0bbf272fc0036fe983
BasilAk-cyber <aubasil07@gmail.com>
2026-08-09 23:02:56 +0100

    Remove Redis password from config

--- src/shared/config/redis.config.ts
+++ src/shared/config/redis.config.ts
@@ -1,6 +1,5 @@
 // src/config/redis.config.ts
 import { registerAs } from '@nestjs/config';
-
 export default registerAs('redisConfig', () => ({
   host: process.env.REDIS_HOST,
   port: parseInt(process.env.REDIS_PORT ?? '6379', 10),
